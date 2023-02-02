31f5251afad0fd82c3e7829bee75c4464786e15b
Eric K <dongwon.kr@gmail.com>
2023-02-02 16:42:21 -0500

    keiko v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MapSectionComponent } from './components/map-section/map-section.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MapSectionComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
